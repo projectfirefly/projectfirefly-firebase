cedc02f9ac499dc2c450ce1b5eb6e4b9b4918ca0
Robert Driskell <driskell.ba@gmail.com>
2019-07-31 19:29:19 -0500

    Got adding everything working, working on updating

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,115 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
+const cors = require("cors");
+
 // Set GOOGLE_APPLICATION_CREDENTIALS env variable to private key path
 // https://firebase.google.com/docs/functions/local-emulator
 admin.initializeApp();
+const db = admin.firestore();
 
-exports.testCollectionCreate = functions.https.onRequest(async (req, res) => {
-    const data = {
-        first_name: "george",
-        last_name: "lopez",
+exports.testUserCreate = functions.https.onRequest(async (req, res) => {
+    const user = {
+        uid: "asdf1234",
+        email: "asdf@asdf.asdf",
     };
-    const db = admin.firestore();
-    const newDoc = await db.collection("asdf").add(data);
-    newDoc.collection("1234").add({ yo: "yo" });
+
+    try {
+        await db
+            .collection("users")
+            .doc(user.uid)
+            .set(
+                {
+                    email: user.email,
+                    first_name: "",
+                    last_name: "",
+                    academic_research: false,
+                },
+                { merge: true }
+            )
+            .then(() => {
+                return db
+                    .collection("users")
+                    .doc(user.uid)
+                    .collection("information")
+                    .add({
+                        city: "",
+                        state: "",
+                        zip: "",
+                        country: "",
+                        marital_status: "",
+                        job: "",
+                        education: "",
+                        age: 0,
+                        children_relation: "",
+                    });
+            })
+            .then(() => {
+                res.status(200).send("Nice");
+            });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+exports.addChild = functions.https.onRequest((req, res) => {
+    //Why is this required?
+    const corsFn = cors();
+    corsFn(req, res, () => {
+        db.collection("users")
+            .doc(req.headers.uid)
+            .collection("children")
+            .add(req.body)
+            .then((docRef) => {
+                db.collection("users")
+                .doc(req.headers.uid)
+                .collection("children")
+                .doc(docRef.id)
+                .collection("avatar")
+                .add({
+                    color: 53,
+                    accessory: 0,
+                    nickname: ""
+                })
+                .then(() => {
+                    res.status(200).send(req.headers.uid);
+                })
+            });
+    });
 });
 
 exports.createUser = functions.auth.user().onCreate(async user => {
-    const db = admin.firestore();
-    // const newDoc = await db.collection("users").add({
-    //     id: user.uid
-    // });
-    console.log(user);
-    const newDoc = await db.collection("users").doc(user.uid).set({
-        first_name: "",
-        last_name: "",
-    })
+    try {
+        await db
+            .collection("users")
+            .doc(user.uid)
+            .set(
+                {
+                    email: user.email,
+                    first_name: "",
+                    last_name: "",
+                    academic_research: false,
+                },
+                { merge: true }
+            )
+            .then(() => {
+                return db
+                    .collection("users")
+                    .doc(user.uid)
+                    .collection("information")
+                    .add({
+                        city: "",
+                        state: "",
+                        zip: "",
+                        country: "",
+                        marital_status: "",
+                        job: "",
+                        education: "",
+                        age: 0,
+                        children_relation: "",
+                    });
+            });
+    } catch (error) {
+        console.log(error);
+    }
 });
